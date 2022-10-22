cf2df57c325c21819be0259ee3124bc1b6cd6b63
cuongtk1002 <tkcuong1002@gmail.com>
2022-10-22 20:51:57 +0700

    square

---
+++ square.js
@@ -0,0 +1,31 @@
+// Tính bình phương của số bất kỳ
+
+let number = prompt("");
+function square(number) {
+let result = number * number;
+    return result;
+}
+let squareOf = square(number);
+document.getElementById("demo").innerHTML = squareOf;
+
+// tính diện tích hình tròn
+
+// let Pi = 3.14;
+// let number = prompt("");
+// function circleArea(number) {
+// let S = number * number * Pi;
+//     return S;
+// }
+// let result = circleArea(number);
+// document.getElementById("demo1").innerHTML = result;
+
+// Tính chu vi hình tròn
+
+// let Pi = 3.14;
+// let number = prompt("");
+// function circleDiameter(number) {
+//     let C = number * 2 * Pi;
+//     return C;
+// }
+// let result = circleDiameter(number);
+// document.getElementById("demo2").innerHTML = result;
